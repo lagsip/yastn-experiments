160b6e2c1556d9dff711bfa18c4539a2e3805916
Tim Klemm lagtop-win <tim.klemm@uni-ulm.de>
2026-01-03 20:50:19 +0100

    chores: renamed translation tests to fit naming convention

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,7 +1,7 @@
 
 def run_all_tests():
-    from . import generator_test_snippet
-    generator_test_snippet.run_tests()
+    from . import test_translation
+    test_translation.run_tests()
 
     from . import test_generate_lindblad
     test_generate_lindblad.run_tests()
